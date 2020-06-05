4339ddacfa10c530c5f8f7bde0ba75e4b6967b1b
Ahmad Thoriq Najahi <najahi@chips-projects.xyz>
2020-06-06 03:07:10 +0700

    aarch64-elf-gcc (GCC) 11.0.0 20200605 (experimental)
    Signed-off-by: Ahmad Thoriq Najahi <najahi@chips-projects.xyz>

---
+++ lib/gcc/aarch64-elf/11.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/najahi/workspace/.build/aarch64-elf/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=aarch64-elf --prefix=/home/najahi/x-tools/aarch64-elf --with-local-prefix=/home/najahi/x-tools/aarch64-elf/aarch64-elf --with-headers=/home/najahi/x-tools/aarch64-elf/aarch64-elf/include --with-newlib --enable-threads=no --disable-shared --enable-__cxa_atexit --disable-libgomp --disable-libmudflap --disable-libmpx --disable-libssp --disable-libquadmath --disable-libquadmath-support --with-gmp=/home/najahi/workspace/.build/aarch64-elf/buildtools --with-mpfr=/home/najahi/workspace/.build/aarch64-elf/buildtools --with-mpc=/home/najahi/workspace/.build/aarch64-elf/buildtools --with-isl=/home/najahi/workspace/.build/aarch64-elf/buildtools --enable-lto --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --enable-target-optspace --disable-nls --disable-multilib --enable-languages=c";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
